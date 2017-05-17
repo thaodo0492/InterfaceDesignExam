87fdce93e469559cf7741b9afedb088b90b8a2db
Shawn <madsshawnhansen@hotmail.com>
2017-05-17 16:03:15 +0200

    ...

--- index.php
+++ index.php
@@ -25,9 +25,25 @@
         <div class="testDivContainer">
             <h1>List of events</h1>
             <div class="content testDivInnerContainer">
-                <div class="testDiv"></div>
-                <div class="testDiv"></div>
-                <div class="testDiv"></div>
+                <div>
+                <div id="imgBackend" class="eventImg">
+                </div>
+                    <div class="eventPreviewDetails">
+                        <div class="eventPreviewDetailsColumnL">
+                            <a>Title:</a>
+                            <a>Time:</a>
+                            <a>Seats available:</a>
+                        </div>
+                        <div class="eventPreviewDetailsColumnR">
+                            <a>PHP Course</a>
+                            <a>18/06 - kl.16:00</a>
+                            <a>48</a>
+                        </div>
+                        <a class="pointer">DETAILS</a>
+                    </div>
+                </div>
+                <div class="eventImg"></div>
+                <div class="eventImg"></div>
             </div>
         </div>
         <div class="testDivContainer">
