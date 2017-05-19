9919fc621f988207d43ba316e78769ae6d0115f1
Shawn <madsshawnhansen@hotmail.com>
2017-05-19 15:22:01 +0200

    ...

--- singleEvent.php
+++ singleEvent.php
@@ -29,7 +29,7 @@
                 <a>21/06</a>
                 <a>11:00-13:50</a>
             </div>
-            <div>
+            <div class="pointer">
                 <p>REGISTER</p>
             </div>
             </div>
@@ -37,14 +37,27 @@
         </div>
 
     </div>
+    
+    <div id="map">
+                    <iframe width="1200" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyDThtVQncVO-lgLbScuMNTThJzKKL_Ekyo&origin=Nørreport&destination=Lygten+37&avoid=tolls|highways" allowfullscreen></iframe>
+                </div>
+    
+    <div class="partnerContainer">
+        <div class="partnerDivInnerContainer">
+            <?php
+                include('showPartners.php');
+            ?>
+        </div>
+
+    </div>
+                
+    <?php
+        include('footer.php');
+    ?>
         
         
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="javaScript.js"></script>
-    
-    <?php
-        include('footer.php');
-    ?>
 </body>
 
 </html>
