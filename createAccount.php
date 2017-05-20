7d41cb4ae4ff4380f4c1a57aee9f75318a966299
Shawn <madsshawnhansen@hotmail.com>
2017-05-20 14:04:12 +0200

    ...

---
+++ createAccount.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Index Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php
+        include('/header.php');
+    ?>
+        
+        
+        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="javaScript.js"></script>
+
+                <?php
+                    include('footer.php');
+                ?>
+</body>
+
+</html>
