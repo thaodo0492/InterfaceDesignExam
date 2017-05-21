36f87384981cd0fb37ae35fc69bb536d349460ab
Shawn <madsshawnhansen@hotmail.com>
2017-05-21 15:26:04 +0200

    ...

---
+++ createEvent.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Create account</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body class="imgEmployee">
+
+    <?php
+        include('/header.php');
+    ?>
+        
+    <form id="formCreateAccount" action="">
+        First name: <input type="text" name="fname"><br>
+        Last name: <input type="text" name="lname"><br>
+        Employee id: <input type="text" name="lname"><br>
+        DOB: <input type="text" name="lname"><br>
+        E-mail: <input type="text" name="lname"><br>
+        Address: <input type="text" name="lname"><br>
+        Phone: <input type="text" name="lname"><br>
+        Date of employment: <input type="text" name="lname"><br>
+        <p id="checkInfo">Send info to employee e-mail <input type="checkbox" value="Bike"></p>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+    <div></div>
+        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="javaScript.js"></script>
+
+                <?php
+                    include('footer.php');
+                ?>
+</body>
+
+</html>
