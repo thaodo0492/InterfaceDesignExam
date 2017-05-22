8e3f11bc5e87897e875b40993aa73935f8024f41
Rot <thaodo0492@yahoo.com>
2017-05-22 16:25:18 +0200

    style

--- events.php
+++ events.php
@@ -184,9 +184,6 @@
         <?php
             include('footer.php');
         ?>
-
-            <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-            <script type="text/javascript" src="js/jquery.dropdown.js"></script>
             <script type="text/javascript">
                 $(function() {
 
--- partners.php
+++ partners.php
@@ -5,6 +5,8 @@
 <head>
     <title>Partners</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Partners</title>
     <?php
         include('/imports.html');
     ?>
